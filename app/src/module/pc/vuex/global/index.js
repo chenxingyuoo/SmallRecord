95b2d81fbdd8f5de2a98e73252bb6fe51031f77b
陈星宇 <chenxy@communet.io>
2018-03-01 15:41:50 +0800

    修复语法错误问题

--- app/src/module/pc/vuex/global/index.js
+++ app/src/module/pc/vuex/global/index.js
@@ -2,9 +2,9 @@
  * Created by chenxingyu on 2017/5/10.
  */
 import state from './state';
-import * as mutations from './mutations';
-import * as getters from './getters';
-import * as actions from './actions';
+import mutations from './mutations';
+import getters from './getters';
+import actions from './actions';
 
 export default {
   state,
